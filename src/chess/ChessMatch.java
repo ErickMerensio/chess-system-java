808d87cbf88162c84c924e27c49126050ca1adab
Erick Gomes <lekogomes2013@gmail.com>
2023-04-11 16:16:34 -0300

    Adicionando as posiçôes da peça no xadrez de forma defensiva

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -26,10 +26,16 @@ public class ChessMatch {
 		}
 
 		private void InitialSetup() {
-			board.placePiece(new Rook(board,Color.White), new Position(0,1));
-			board.placePiece(new Rook(board,Color.Black), new Position(7,1));
-			board.placePiece(new King(board,Color.Black), new Position(0,4));
-			board.placePiece(new King(board,Color.White), new Position(7,4));
+			placeNewPiece('b',1,new Rook(board,Color.White));
+			placeNewPiece('g',1,new Rook(board,Color.White));
+			placeNewPiece('b',8,new Rook(board,Color.Black));
+			placeNewPiece('g',8,new Rook(board,Color.Black));
+			placeNewPiece('e',8,new King(board,Color.Black));
+			placeNewPiece('e',1,new King(board,Color.White));
 			
+		}
+		
+		private void placeNewPiece(char column, int row, ChessPiece piece) {
+			board.placePiece(piece, new ChessPosition(column,row).toPosition());
 		}
 }
