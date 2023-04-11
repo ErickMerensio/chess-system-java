77b972b9a0359e8ce102e80cc286647051e1de51
Erick Gomes <lekogomes2013@gmail.com>
2023-04-10 21:07:09 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 	
 	public static void main(String[] args) {
 		
-			System.out.println("Hellow word");
+		Position pos = new Position(3,5);
+		
 		
 	}
 
