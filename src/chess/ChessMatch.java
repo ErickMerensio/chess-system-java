c5df2ff875d10d5a5b2baa0744bf9e6af256aaa9
Erick Gomes <lekogomes2013@gmail.com>
2023-04-11 14:47:27 -0300

    Adicionando peças no tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	
@@ -9,6 +12,7 @@ public class ChessMatch {
 		
 		public ChessMatch() {
 			board = new Board(8,8);
+			InitialSetup();
 		}
 		
 		public ChessPiece[][] getPieces() {
@@ -21,4 +25,11 @@ public class ChessMatch {
 			return mat;
 		}
 
+		private void InitialSetup() {
+			board.placePiece(new Rook(board,Color.White), new Position(0,1));
+			board.placePiece(new Rook(board,Color.Black), new Position(7,1));
+			board.placePiece(new King(board,Color.Black), new Position(0,4));
+			board.placePiece(new King(board,Color.White), new Position(7,4));
+			
+		}
 }
