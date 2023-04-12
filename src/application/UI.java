6a907851304f9993b96e1c8967874854de5417e0
Erick Gomes <lekogomes2013@gmail.com>
2023-04-12 13:40:31 -0300

    Mudando a cor da peça

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,9 +1,10 @@
 package application;
 
 import chess.ChessPiece;
+import chess.Color;
 
 public class UI {
-	
+
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_RED = "\u001B[31m";
@@ -22,25 +23,31 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-	
-		public static void printBoard(ChessPiece [][] pieces) {
-			
-			for (int i=0; i<pieces.length; i++) {
-				System.out.print((8 - i) + " ");
-				for(int j=0; j<pieces.length; j++) {	
-					printPiece(pieces[i][j]);
-				}
-				System.out.println();
+
+	public static void printBoard(ChessPiece[][] pieces) {
+
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j]);
 			}
-			System.out.println("  a b c d e f g h");
+			System.out.println();
 		}
-		
-		private static void printPiece(ChessPiece piece) {
-			if (piece == null) {
-				System.out.print("-");
-			}else {
-				System.out.print(piece);
-		}
-		System.out.print(" ");
-		}
+		System.out.println("  a b c d e f g h");
+	}
+
+	private static void printPiece(ChessPiece piece) {
+		if (piece == null) {
+            System.out.print("-");
+        }
+        else {
+            if (piece.getColor() == Color.White) {
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            }
+            else {
+                System.out.print(ANSI_RED + piece + ANSI_RESET);
+            }
+        }
+        System.out.print(" ");
+	}
 }
